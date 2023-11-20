c2023ccb2d8e446fa485b153d8d4e84d46d8d6ca
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-20 17:33:51 -0500

    Wrote and tested functionality for all methods

--- graph.js
+++ graph.js
@@ -11,25 +11,77 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex)
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for(const vertex of vertexArray) {
+      this.addVertex(vertex)
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2)
+    v2.adjacent.add(v1)
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2)
+    v2.adjacent.delete(v1)
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (const neighbor of vertex.adjacent) {
+      vertex.adjacent.delete(neighbor)
+    }
+
+    this.nodes.delete(vertex)
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const toVisitStack = [start]
+    const seen = new Set(toVisitStack)
+    const seenVals = []
+
+    while (toVisitStack.length) {
+      const current = toVisitStack.pop()
+      seenVals.push(current.value)
+      for (const neighbor of current.adjacent) {
+        if (!seen.has(neighbor)){
+          seen.add(neighbor)
+          toVisitStack.push(neighbor)
+        }
+      }
+    }
+
+    return seenVals
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const toVisitQueue = [start]
+    const seen = new Set(toVisitQueue)
+    const seenVals = []
+
+    while (toVisitQueue.length) {
+      const current = toVisitQueue.shift()
+      seenVals.push(current.value)
+      for (const neighbor of current.adjacent) {
+        if (!seen.has(neighbor)){
+          seen.add(neighbor)
+          toVisitQueue.push(neighbor)
+        }
+      }
+    }
+
+    return seenVals
+  }
 }
 
 module.exports = {Graph, Node}
